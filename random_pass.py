3a4066f9ffd9ceb9a4bd707a2758bb383c903d6f
Evgen2325 <111738565+Evgen2325@users.noreply.github.com>
2022-10-07 18:03:05 +0300

    Delete random_pass.py

--- random_pass.py
+++
@@ -1,8 +0,0 @@
-import random
-
-symbols = '1234567890qwertyuiopasdfghjklzxcvbnm'
-lenght = input('Input lenght password please: \n')
-password = ''
-for i in range(int(lenght)):
-    password += random.choice(symbols)
-print(password)
